65b38c9d1fa21308f47f9e8530273f98fde69a21
HHallb <hakan.hallb@protonmail.com>
2025-09-15 17:54:57 +0200

    Added additional comments to theexample notebook ex01

--- examples/ex01_grain_growth.py
+++ examples/ex01_grain_growth.py
@@ -7,8 +7,8 @@ import numpy as np
 import time
 import torch
 
-# Set pyPFC parameters
-# ====================
+# Set pyPFC parameters, these are handled as a dictionary
+# =======================================================
 params = {
     'dtime':                  1.0e-3,                 # Time increment
     'struct':                 'FCC',                  # Set crystal structure
@@ -20,7 +20,7 @@ params = {
     'normalize_pf':           True,                   # Normalize the phase fields to [0,1], or not
     'update_scheme':          '1st_order',            # Time integration scheme
     'update_scheme_params':   None,                   # Parameters in the 2nd order time integration scheme: alpha, beta, gamma
-    'device_type':            'cpu',#'gpu',                  # PyTorch device (CPU/GPU)
+    'device_type':            'gpu',                  # PyTorch device (CPU/GPU)
     'device_number':          0,                      # GPU device number (if multiple GPUs are available)
     'dtype_cpu':              np.double,              # Set precision of numpy arrays
     'dtype_gpu':              torch.float64,          # Set precision of PyTorch tensors
@@ -40,22 +40,16 @@ nstep            = 6000                               # Number of simulation ste
 nout             = 500                                # Evaluate step data in every nout:h step
 n_save_step_data = 1000                               # Save step data in every n_save_step_data:th step
 nfill            = 7                                  # Number of figures to use in filenames (pre-pad with zeroes if needed)
-output_path      = '/home/hlhh/Insync/OneDriveLTH/python/pyPFC/examples/ex01_output/' # Output path
+output_path      = '/home/hlhh/Insync/OneDriveLTH/python/pyPFC/examples/ex01_output_gpu_96x96x96/' # Output path
 output_file      = 'pypfc_setup.txt'                                                  # Output file name
 
 # Define the computational grid
 # =============================
-dSize          = params['alat'] * np.array([64, 64, 64], dtype=float)
-ndiv           = 8 * np.array([64, 64, 64], dtype=int)
-#dSize          = params['alat'] * np.array([128, 128, 128], dtype=float)
-#ndiv           = 8 * np.array([128, 128, 128], dtype=int)
-ddiv           = dSize / ndiv
-
-# Evaluate and display the grid parameters
-# ========================================
-#nx,ny,nz = ndiv
-#dx,dy,dz = ddiv
-#dSize    = ndiv * ddiv
+#dSize          = params['alat'] * np.array([64, 64, 64], dtype=float)   # Domain size along the x, y and z axes
+#ndiv           = 8 * np.array([64, 64, 64], dtype=int)                  # Number of grid divisions along the x, y and z axes       
+dSize          = params['alat'] * np.array([96, 96, 96], dtype=float)
+ndiv           = 8 * np.array([96, 96, 96], dtype=int)
+ddiv           = dSize / ndiv                                           # Grid spacing along the x, y and z axes
 print(f'ndiv:    {ndiv}')
 print(f'ddiv:    {ddiv} [a]')
 print(f'dSize:   {dSize} [a]')
@@ -67,82 +61,75 @@ pypfc = pypfc.setup_simulation(ndiv, ddiv, config=params)
 
 # Save setup information to file
 # ==============================
-pypfc.write_info_file(output_path+output_file)
+pypfc.write_info_file(output_path+output_file)  # Write setup information to a text file
 
 # Create a preprocessor object and generate the initial density field:
 # A centered spherical nucleus in an otherwise liquid domain
 # ====================================================================
-xtalRot    = np.eye(3, dtype=float)
-xtalRadius = 0.1 * min(dSize)  # Radius of the spherical nucleus
-den        = pypfc.do_single_crystal(xtalRot, [xtalRadius])
-pypfc.set_density(den) # Sets the new density field in the pypfc simulation object
+xtalRot    = np.eye(3, dtype=float)                         # No rotation of the seed crystal
+xtalRadius = 0.1 * min(dSize)                               # Radius of the spherical seed crystal
+den        = pypfc.do_single_crystal(xtalRot, [xtalRadius]) # Generates a single crystal in the center of the domain
+pypfc.set_density(den)                                      # Sets the new density field in the pyPFC simulation object
 
 # Evaluate energy
 # ===============
-ene, mean_ene = pypfc.get_energy()
+ene, mean_ene = pypfc.get_energy()  # Evaluate the PFC free energy and its mean value
 
 # Evaluate phase field
 # ====================
-pf = pypfc.get_phase_field()
-pf_verts, pf_faces, volume = pypfc.get_phase_field_contour(pf)
+pf = pypfc.get_phase_field()                                    # Evaluate the phase field
+pf_verts, pf_faces, volume = pypfc.get_phase_field_contour(pf)  # Evaluate the phase field contour and the volume contained within it
 
 # Interpolate density field maxima
 # ================================
-atom_coord, atom_data = pypfc.interpolate_density_maxima(den, ene, pf)
-natoms = atom_coord.shape[0] # Retrieve the number of atoms (= density peaks)
+atom_coord, atom_data = pypfc.interpolate_density_maxima(den, ene, pf)  # Interpolate density maxima positions and associated data (density, energy, phase field)
+natoms = atom_coord.shape[0]                                            # Retrieve the number of atoms (= density peaks)
 
 # Save data to VTK files
 # ======================
 plotnr   = str(0).zfill(nfill)
 filename = output_path + 'pfc_data_' + plotnr
-pypfc.write_vtk_structured_grid(filename, [den], ['den'])
-pypfc.write_vtk_points(filename, atom_coord, [atom_data[:,0], atom_data[:,1], atom_data[:,2]], ['den', 'ene', 'pf'])
-
-# Variable initialization
-# =======================
-start_step       = 0
-step             = 0
-total_time       = 0.0
-state_output_idx = 0
+pypfc.write_vtk_structured_grid(filename, [den], ['den'])                                                               # Save the continuous density field to structured grid VTK file
+pypfc.write_vtk_points(filename, atom_coord, [atom_data[:,0], atom_data[:,1], atom_data[:,2]], ['den', 'ene', 'pf'])    # Save the discrete density maxima (atoms) to VTK point file
 
 # Prepare storage of state data and save the intial state
 # =======================================================
-state_output       = np.zeros((nstep+1, 6), dtype=float)
-den, mean_den      = pypfc.get_density()
-ene, mean_ene      = pypfc.get_energy()
-state_output[0,:]  = [0.0, natoms, mean_ene, mean_den, volume, 0.0]
-state_output_idx  += 1
+total_time         = 0.0                                            # Initialize total simulation time
+state_output_idx   = 0                                              # Initialize state output index
+state_output       = np.zeros((nstep+1, 6), dtype=float)            # Allocate array for state data: time, natoms, mean_ene, mean_den, volume, cpu_time
+_ , mean_den       = pypfc.get_density()                            # Evaluate the mean density
+state_output[0,:]  = [0.0, natoms, mean_ene, mean_den, volume, 0.0] # Save initial state data
+state_output_idx  += 1                                              # Step up state output index
 
 # Evolve density field
 # ====================
-tstart = time.time() # Start timer
+tstart = time.time()        # Start timer
+pypfc.set_verbose(False)    # Turn off verbose output during the time step loop
 
-# Turn off verbose output during the time step loop
-pypfc.set_verbose(False)
-
-for step in range(start_step,nstep):
+for step in range(nstep):
 
     # Update density
     pypfc.do_step_update()
 
     # Step up timer
-    total_time += params['dtime']
+    total_time += params['dtime']   
 
+    # Evaluate and save data at specified intervals
     if np.mod(step+1,nout)==0 or step+1==nstep:
 
         # Evaluate data in the current step
         # =================================
-        den, mean_den              = pypfc.get_density()
-        ene, mean_ene              = pypfc.get_energy()
-        pf                         = pypfc.get_phase_field()
-        pf_verts, pf_faces, volume = pypfc.get_phase_field_contour(pf)
-        atom_coord, atom_data      = pypfc.interpolate_density_maxima(den, ene, pf)
-        natoms = atom_coord.shape[0]
+        den, mean_den              = pypfc.get_density()                            # Evaluate the density field and its mean value
+        ene, mean_ene              = pypfc.get_energy()                             # Evaluate the PFC free energy and its mean value
+        pf                         = pypfc.get_phase_field()                        # Evaluate the phase field
+        pf_verts, pf_faces, volume = pypfc.get_phase_field_contour(pf)              # Evaluate the phase field contour and the volume contained within it
+        atom_coord, atom_data      = pypfc.interpolate_density_maxima(den, ene, pf) # Interpolate density maxima positions and associated data (density, energy, phase field)
+        natoms = atom_coord.shape[0]                                                # Retrieve the number of atoms (= density peaks)
 
         # Save state data
         # ===============
-        state_output[state_output_idx,:] = [total_time, natoms, mean_ene, mean_den, volume, time.time()-tstart]
-        state_output_idx += 1
+        state_output[state_output_idx,:] = [total_time, natoms, mean_ene, mean_den, volume, time.time()-tstart] # Save state data
+        state_output_idx += 1                                                                                   # Step up state output index
 
         # Print state to identify the simulation progress
         # ===============================================
@@ -156,6 +143,9 @@ for step in range(start_step,nstep):
         # Save step data
         # ===============
         if np.mod(step+1,n_save_step_data)==0:
+
+            # Save data to a binary pickle file
+            # =================================
             filename = output_path + 'step_' + str(step+1).zfill(nfill)
             pypfc.save_pickle(filename, [ step, total_time, ndiv, ddiv, dSize, den, state_output[:state_output_idx+1,:]])
 
